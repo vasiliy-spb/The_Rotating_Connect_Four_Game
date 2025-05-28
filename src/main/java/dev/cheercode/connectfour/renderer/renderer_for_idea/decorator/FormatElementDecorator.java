38c302b00a2f9b51059f03ec092e9e38a0b7cb9d
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-05-28 16:56:07 +0700

    Fix bad representation in windows

--- src/main/java/dev/cheercode/connectfour/renderer/renderer_for_idea/decorator/FormatElementDecorator.java
+++ src/main/java/dev/cheercode/connectfour/renderer/renderer_for_idea/decorator/FormatElementDecorator.java
@@ -3,15 +3,15 @@ package dev.cheercode.connectfour.renderer.renderer_for_idea.decorator;
 import dev.cheercode.connectfour.renderer.renderer_for_idea.Element;
 
 public class FormatableElementDecorator extends ElementDecorator {
-    private final String formattedValue;
+    private final String template;
 
     public FormatableElementDecorator(Element element, String template) {
         super(element);
-        this.formattedValue = String.format(template, element.getValue());
+        this.template = template;
     }
 
     @Override
     public String getValue() {
-        return formattedValue;
+        return String.format(template, element.getValue());
     }
 }
